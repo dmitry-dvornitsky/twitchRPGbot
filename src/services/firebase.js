e2189ce380fd8d1c0691ca182eeabb80b252451a
dinozav <dinozav1@yandex.ru>
2018-12-27 21:13:20 +0100

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,12 +2,7 @@ const firebase = require("firebase/app");
 require("firebase/database");
 
 const config = {
-  apiKey: "AIzaSyCLz4UPEFhfFL5zgm-FSo9_zA2JA4Gtaac",
-  authDomain: "twitch-rpg-bot.firebaseapp.com",
-  databaseURL: "https://twitch-rpg-bot.firebaseio.com",
-  projectId: "twitch-rpg-bot",
-  storageBucket: "",
-  messagingSenderId: "930571059890"
+
 };
 
 module.exports = firebase.initializeApp(config);
